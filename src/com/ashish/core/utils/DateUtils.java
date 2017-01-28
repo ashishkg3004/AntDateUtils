68a97c851d29169141eeb5837710365abc329494
Ashishkumar Gupta <adoreashish@gmail.com>
2017-01-28 18:38:23 +0530

    Completed Ant – How To Create A Jar File with external libraries

--- src/com/ashish/core/utils/DateUtils.java
+++ src/com/ashish/core/utils/DateUtils.java
@@ -1,15 +1,25 @@
 package com.ashish.core.utils;
 
-import java.util.Date;
+import org.joda.time.LocalDate;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class DateUtils {
 
-	public static void main(String args[]) {
+	private static final Logger logger = LoggerFactory.getLogger(DateUtils.class);
+
+	public static void main(String[] args) {
+
+		logger.debug("[MAIN] Current Date : {}", getLocalCurrentDate());
 		System.out.println(getLocalCurrentDate());
+
 	}
 
-	private static Date getLocalCurrentDate() {
-		return new Date();
+	private static String getLocalCurrentDate() {
+
+		LocalDate date = new LocalDate();
+		return date.toString();
+
 	}
 
 }
